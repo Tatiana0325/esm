7c9087b1977656d915936abc01ab327d34d79d0a
Tatiana Krasnova <krasnova-tatyana92@mail.ru>
2020-06-25 15:36:50 +0300

    first commit

---
+++ src/game.js
@@ -0,0 +1,19 @@
+class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+
+  const defaultGame = new Game();
+  export default defaultGame;
+  
+  export class GameSavingData {
+  }
+  
+  export function readGameSaving() {
+  }
+  
+  export function writeGameSaving() {
+  }
+
+  import defaultCharacter from './domain';
